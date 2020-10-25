43b198d9f9d8ef2ce0e275efe92659c476baedca
freja <s195473@student.dtu.dk>
2020-10-26 00:48:05 +0100

    CalenderView oprettet + MainActivity <framework ændret

--- app/src/main/java/dtu/android/moroapp/MinKalenderFragment.java
+++ app/src/main/java/dtu/android/moroapp/MinKalenderFragment.java
@@ -1,5 +1,6 @@
 package dtu.android.moroapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +9,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-class MinKalenderFragment : Fragment(){
+public class MinKalenderFragment extends Fragment implements View.OnClickListener {
 
-        }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_min_kalender, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+    //TO-DO
+    }
 }
