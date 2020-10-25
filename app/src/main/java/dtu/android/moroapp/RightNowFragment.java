8c7e19496c83a1f86ae56b0dc004178d6482aeff
Oliver Comprés Poulsen <oli@live.dk>
2020-10-25 23:32:08 +0100

    Added files for right now and eventcard

---
+++ app/src/main/java/dtu/android/moroapp/RightNowFragment.java
@@ -0,0 +1,17 @@
+package dtu.android.moroapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class RightNowFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.right_now_fragment, container, false);
+    }
+}
