87b6e6fdd2330c50dda4c406cfad1f57a5068f79
Daniel <s143861@student.dtu.dk>
2020-10-26 14:01:14 +0100

    Try to set up nav

--- app/src/main/java/dtu/android/moroapp/FrontPageFragment.kt
+++ app/src/main/java/dtu/android/moroapp/FrontPageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +21,7 @@ private const val ARG_PARAM2 = "param2"
 class FrontPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var liste: View
+    private lateinit var findEvent_Button: Button
     private var param1: String? = null
     private var param2: String? = null
 
@@ -36,6 +38,9 @@ class FrontPageFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_front_page, container, false)
         liste = view.findViewById(R.id.liste)
+        findEvent_Button = view.findViewById(R.id.find_event_button)
+
+        
         return view
     }
 
--- app/src/main/java/dtu/android/moroapp/MainActivity.java
+++ app/src/main/java/dtu/android/moroapp/MainActivity.java
@@ -1,14 +1,26 @@
 package dtu.android.moroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        if (savedInstanceState == null){
+            Fragment fragment = new FrontPageFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.mainFragment, fragment).commit();
+
+        }
+
     }
 }
