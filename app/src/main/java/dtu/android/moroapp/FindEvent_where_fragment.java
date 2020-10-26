6055bc288dab88ed47f369923b5cb09020151975
Thomas <thomas8@hohnen.dk>
2020-10-26 16:13:18 +0100

    Worked on the op and down buttons. Thy dont really work now but they did before

--- app/src/main/java/dtu/android/moroapp/FindEvent_where_fragment.java
+++ app/src/main/java/dtu/android/moroapp/FindEvent_where_fragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class FindEvent_where_fragment extends Fragment {
+public class FindEvent_where_fragment extends Fragment implements View.OnClickListener{
+
+    private Button ButtonOp_where, ButtonDown_where;
+    private View root;
 
     public FindEvent_where_fragment() {
         // Required empty public constructor
@@ -16,6 +23,31 @@ public class FindEvent_where_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_event_where_fragment, container, false);
+        root = inflater.inflate(R.layout.fragment_find_event_where_fragment, container, false);
+
+        ButtonOp_where = root.findViewById(R.id.button_Op_where);
+        ButtonDown_where = root.findViewById(R.id.button_Down_what);
+
+        ButtonOp_where.setOnClickListener(this);
+        ButtonDown_where.setOnClickListener(this);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == ButtonOp_where) {
+            findEvent_what_fragment newFragment = new findEvent_what_fragment();
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.mainFragment, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
     }
 }
--- app/src/main/java/dtu/android/moroapp/findEvent_what_fragment.java
+++ app/src/main/java/dtu/android/moroapp/findEvent_what_fragment.java
@@ -2,14 +2,20 @@ package dtu.android.moroapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class findEvent_what_fragment extends Fragment {
+public class findEvent_what_fragment extends Fragment implements View.OnClickListener {
 
+    private Button ButtonOp_what, ButtonDown_what;
+    private View root;
 
     public findEvent_what_fragment() {
         // Required empty public constructor
@@ -18,6 +24,38 @@ public class findEvent_what_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_event_what_fragment, container, false);
+        root =inflater.inflate(R.layout.fragment_find_event_what_fragment, container, false);
+
+        ButtonOp_what = root.findViewById(R.id.button_Op_what);
+        ButtonDown_what = root.findViewById(R.id.button_Down_what);
+
+        ButtonOp_what.setOnClickListener(this);
+        ButtonDown_what.setOnClickListener(this);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == ButtonOp_what){
+            FindEvent_when_fragment newFragment = new FindEvent_when_fragment();
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.mainFragment, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        else if(view == ButtonDown_what){
+            FindEvent_where_fragment newFragment = new FindEvent_where_fragment();
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.mainFragment, newFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
     }
 }
