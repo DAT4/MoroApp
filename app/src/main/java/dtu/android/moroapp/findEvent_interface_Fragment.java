2b3534de31c16fd98fb618a8e223010f066c5f91
Daniel <s143861@student.dtu.dk>
2020-10-26 09:51:57 +0100

    added more fragments

--- app/src/main/java/dtu/android/moroapp/findEvent_interface_Fragment.java
+++ app/src/main/java/dtu/android/moroapp/findEvent_interface_Fragment.java
@@ -10,15 +10,15 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link findEventFragment#newInstance} factory method to
+ * Use the {@link findEvent_interface_Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class findEventFragment extends Fragment {
+public class findEvent_interface_Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_event, container, false);
+        return inflater.inflate(R.layout.fragment_find_event_interface, container, false);
     }
 }
