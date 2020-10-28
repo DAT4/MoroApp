fb8313f8d470e45e249377ce7bda908f9ceea444
freja <s195473@student.dtu.dk>
2020-10-29 00:25:57 +0100

    Min Profil virker fandme

--- app/src/main/java/dtu/android/moroapp/KommendeEventsFragment.java
+++ app/src/main/java/dtu/android/moroapp/KommendeEventsFragment.java
@@ -10,14 +10,13 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AnbefaledeFragment#newInstance} factory method to
+ * Use the {@link KommendeEventsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AnbefaledeFragment extends Fragment {
-
+public class KommendeEventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anbefalede, container, false);
+        return inflater.inflate(R.layout.fragment_kommende_events, container, false);
     }
 }
