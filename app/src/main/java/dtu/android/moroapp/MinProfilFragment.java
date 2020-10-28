8ee428ae513ac2f9fc990c21f2bf6940e1a0c627
freja <s195473@student.dtu.dk>
2020-10-28 16:20:53 +0100

    Min profil fragment fungerer

--- app/src/main/java/dtu/android/moroapp/MinProfilFragment.java
+++ app/src/main/java/dtu/android/moroapp/MinProfilFragment.java
@@ -13,52 +13,22 @@ import android.view.ViewGroup;
  * Use the {@link MinProfilFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MinProfilFragment extends Fragment {
+public class MinProfilFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MinProfilFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MinProfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MinProfilFragment newInstance(String param1, String param2) {
-        MinProfilFragment fragment = new MinProfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            super.onCreate(savedInstanceState);
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_min_profil, container, false);
-    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_min_profil, container, false);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //TO-DO
+        }
 }
