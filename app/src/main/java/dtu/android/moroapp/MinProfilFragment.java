3e5e621b035fdab93831d955401280d4614723c7
freja <s195473@student.dtu.dk>
2020-10-28 16:10:39 +0100

    Kalender in progress part 2

---
+++ app/src/main/java/dtu/android/moroapp/MinProfilFragment.java
@@ -0,0 +1,34 @@
+package dtu.android.moroapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MinProfilFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MinProfilFragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_min_profil, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //TO-DO
+    }
+}
