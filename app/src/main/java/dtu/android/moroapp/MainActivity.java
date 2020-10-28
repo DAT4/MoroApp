a95840502e466c89e7322850d1198a5328de3deb
Daniel <s143861@student.dtu.dk>
2020-10-28 14:24:07 +0100

    Added right now to nav

--- app/src/main/java/dtu/android/moroapp/MainActivity.java
+++ app/src/main/java/dtu/android/moroapp/MainActivity.java
@@ -4,23 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button bRightNow, bHome;
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        bRightNow = (Button) findViewById(R.id.nav_now);
+        bHome = (Button) findViewById(R.id.nav_home);
 
-        if (savedInstanceState == null){
+
+        bRightNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rightNow();
+            }
+        });
+
+        bHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new FrontPageFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.mainFragment, fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
+
+        if (savedInstanceState == null) {
             Fragment fragment = new FrontPageFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.mainFragment, fragment).commit();
 
         }
 
+    }
+
+    private void rightNow() {
+        Fragment fragment = new RightNowFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainFragment, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
--- app/src/main/java/dtu/android/moroapp/RightNowFragment.java
+++ app/src/main/java/dtu/android/moroapp/RightNowFragment.java
@@ -1,17 +1,37 @@
 package dtu.android.moroapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class RightNowFragment extends Fragment {
+
+    private Button back;
+    private View root;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.right_now_fragment, container, false);
+        root = inflater.inflate(R.layout.right_now_fragment, container, false);
+
+        back = root.findViewById(R.id.right_now_back);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class );
+                startActivity(intent);
+                }
+            }
+        );
+
+        return root;
     }
 }
