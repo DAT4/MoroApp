8672b0fafc7dd28b4aaaed49de026ac6ac4aa3ce
Thomas <thomas8@hohnen.dk>
2020-11-23 13:48:53 +0100

    Made the adapter for the FrontPage

---
+++ app/src/main/java/dtu/android/moroapp/models/Frontpage_Event_Model.java
@@ -0,0 +1,38 @@
+package dtu.android.moroapp.models;
+
+public class Frontpage_Event_Model {
+
+    String title;
+    String description;
+    int image;
+
+    public Frontpage_Event_Model(String title, String description, int image) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
