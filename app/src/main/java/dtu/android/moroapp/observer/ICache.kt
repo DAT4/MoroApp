1739da512e1c38692e97ec2beedf90f9cfd368a8
martin <s195469@student.dtu.dk>
2020-12-01 00:58:01 +0100

    Error handling added

--- app/src/main/java/dtu/android/moroapp/observer/ICache.kt
+++ app/src/main/java/dtu/android/moroapp/observer/ICache.kt
@@ -1,22 +1,29 @@
 package dtu.android.moroapp.observer
 
+import android.widget.Toast
+import dtu.android.moroapp.utils.GraphQL
 import dtu.android.moroapp.utils.Query
 import dtu.android.moroapp.utils.Response
 import dtu.android.moroapp.utils.postStuff
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.lang.NullPointerException
 
 interface ICache : IObservable {
     val url: String
 
     var content: MutableList<*>
 
-    fun cache(query: Query) {
+    fun cache(query: GraphQL) {
         GlobalScope.launch(Dispatchers.IO) {
             val data: Response = postStuff(query, url)
             launch(Dispatchers.Main) {
-                this@ICache.content = data.data.events as MutableList<*>
+                try {
+                    this@ICache.content = data.data.events as MutableList<*>
+                } catch (e: NullPointerException) {
+                    println("Intet net")
+                }
                 sendUpdateEvent()
             }
         }
