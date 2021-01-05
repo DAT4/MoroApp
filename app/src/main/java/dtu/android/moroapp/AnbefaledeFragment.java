ea8eb5e2ab9e7a50b131d119e19bd35472d3f872
mocompres <oli@live.dk>
2021-01-05 14:46:24 +0100

    implemented eventview manager and layout for my profil

--- app/src/main/java/dtu/android/moroapp/AnbefaledeFragment.java
+++ app/src/main/java/dtu/android/moroapp/AnbefaledeFragment.java
@@ -3,21 +3,91 @@ package dtu.android.moroapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
+
+import dtu.android.moroapp.adapters.EventsViewManager;
+import dtu.android.moroapp.models.Event;
+import dtu.android.moroapp.observer.ConcreteEvents;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AnbefaledeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AnbefaledeFragment extends Fragment {
+public class AnbefaledeFragment extends Fragment implements View.OnClickListener {
+
+    private View root;
+    RecyclerView recyclerView;
+    EventsViewManager eventsViewManager;
+    Button btnList, btnGrid, btnMap;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anbefalede, container, false);
+        root = inflater.inflate(R.layout.fragment_anbefalede, container, false);
+
+        // Events
+        List<Event> events = (List<Event>) ConcreteEvents.INSTANCE.getAllEvents();
+
+        // Manager setup
+        eventsViewManager = new EventsViewManager(events);
+
+        // BTN setup
+        btnList = root.findViewById(R.id.recommendEvents_list_view);
+        btnList.setOnClickListener(this);
+
+        btnGrid = root.findViewById(R.id.recommendEvents_grid_view);
+        btnGrid.setOnClickListener(this);
+
+        btnMap = root.findViewById(R.id.recommendEvents_map_view);
+        btnMap.setOnClickListener(this);
+
+        recyclerView = root.findViewById(R.id.recommendRecyclerView);
+        updateView();
+
+        return root;
+    }
+
+    void viewList(View v) {
+        eventsViewManager.viewList(recyclerView, this.getContext());
+    }
+
+    void viewGrid(View v) {
+
+        eventsViewManager.viewGrid(recyclerView, this.getContext());
+    }
+
+    void viewMap(View v) {
+        eventsViewManager.viewMap(recyclerView, this.getContext());
+    }
+
+    void updateView() {
+        recyclerView.setAdapter(eventsViewManager.getAdapter());
+        recyclerView.setLayoutManager(eventsViewManager.getLayoutManager(this.getContext()));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.recommendEvents_list_view:
+                viewList(v);
+                break;
+            case R.id.recommendEvents_grid_view:
+                viewGrid(v);
+                break;
+            case R.id.recommendEvents_map_view:
+                viewMap(v);
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/dtu/android/moroapp/SavedEventsFragment.java
+++ app/src/main/java/dtu/android/moroapp/SavedEventsFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,16 +16,29 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dtu.android.moroapp.adapters.EventsViewManager;
+import dtu.android.moroapp.models.Event;
+import dtu.android.moroapp.observer.ConcreteEvents;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SavedEventsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SavedEventsFragment extends Fragment {
+public class SavedEventsFragment extends Fragment implements View.OnClickListener {
 
     private View view;
+    Button btnList, btnGrid, btnMap;
+    RecyclerView recyclerView;
+    EventsViewManager eventsViewManager;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_saved_events, container, false);
 
   /*      ImageView imageListView = view.findViewById(R.id.savedEvents_list_view);
@@ -35,8 +49,64 @@ public class SavedEventsFragment extends Fragment {
         imageGridView.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorIconOrange));
         imageMapView.setColorFilter(ContextCompat.getColor(getContext(), R.color.colorIconOrange)); */
 
+        // BTN setup
+        btnList = view.findViewById(R.id.savedEvents_list_view);
+        btnList.setOnClickListener(this);
 
-        // Inflate the layout for this fragment
+        btnGrid = view.findViewById(R.id.savedEvents_grid_view);
+        btnGrid.setOnClickListener(this);
+
+        btnMap = view.findViewById(R.id.savedEvents_map_view);
+        btnMap.setOnClickListener(this);
+
+        // load saved events events
+        List<Event> events = new ArrayList<>();
+        events.add(ConcreteEvents.INSTANCE.getAllEvents().get(2));
+
+
+        // Manger setup
+        eventsViewManager = new EventsViewManager(events);
+
+        // recycler view setup
+        recyclerView = view.findViewById(R.id.savedEventsRecyclerView);
+        updateView();
+
         return view;
     }
+
+    private void updateView() {
+        recyclerView.setAdapter(eventsViewManager.getAdapter());
+        recyclerView.setLayoutManager(eventsViewManager.getLayoutManager(this.getContext()));
     }
+
+    void viewList(View v) {
+        eventsViewManager.viewList(recyclerView, this.getContext());
+    }
+
+    void viewGrid(View v) {
+
+        eventsViewManager.viewGrid(recyclerView, this.getContext());
+    }
+
+    void viewMap(View v) {
+        eventsViewManager.viewMap(recyclerView, this.getContext());
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.savedEvents_list_view:
+                viewList(v);
+                break;
+            case R.id.savedEvents_grid_view:
+                viewGrid(v);
+                break;
+            case R.id.savedEvents_map_view:
+                viewMap(v);
+                break;
+            default:
+                break;
+        }
+    }
+}
