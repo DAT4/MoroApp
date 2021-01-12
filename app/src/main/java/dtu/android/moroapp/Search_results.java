fbadd443a67b449a540dfdf555240a67ed4d383a
unknown <s143861@student.dtu.dk>
2021-01-12 10:50:28 +0100

    Fixed constraints

--- app/src/main/java/dtu/android/moroapp/Search_results.java
+++ app/src/main/java/dtu/android/moroapp/Search_results.java
@@ -2,15 +2,14 @@ package dtu.android.moroapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
