45f69f310f342b0941ea15ebaff23e186f2b0550
Thomas <thomas8@hohnen.dk>
2021-01-13 20:19:29 +0100

    Now the the profile button in the nav bar changes to the OverViewMyProfile fragment that contains the myProfile fragment

--- app/src/main/java/dtu/android/moroapp/OverViewMyProfile.java
+++ app/src/main/java/dtu/android/moroapp/OverViewMyProfile.java
@@ -2,18 +2,36 @@ package dtu.android.moroapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import dtu.android.moroapp.databinding.FragmentOverViewMyProfileBinding;
 
 public class OverViewMyProfile extends Fragment {
 
+    Button myProfile;
+    Button calender;
+    private FragmentOverViewMyProfileBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentOverViewMyProfileBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+
+
     }
 
 }
