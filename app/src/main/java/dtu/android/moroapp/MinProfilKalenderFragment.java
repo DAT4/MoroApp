f59a796bac2074934af573cc5a5ac8c965d8af73
Thomas <thomas8@hohnen.dk>
2021-01-14 13:40:11 +0100

    Wokring on custemizing the calender

--- app/src/main/java/dtu/android/moroapp/MinProfilKalenderFragment.java
+++ app/src/main/java/dtu/android/moroapp/MinProfilKalenderFragment.java
@@ -7,17 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 public class MinProfilKalenderFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    View root;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_min_profil_kalender, container, false);
+        root = inflater.inflate(R.layout.fragment_min_profil_kalender, container, false);
+
+        CalendarView calender = root.findViewById(R.id.profileCalender);
+        calender.setFirstDayOfWeek(2);
+
+        return root;
     }
 }
