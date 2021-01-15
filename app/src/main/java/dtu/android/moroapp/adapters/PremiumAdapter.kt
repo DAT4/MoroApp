a2fcef98a7e277a2b4e0c756b050921a879f603d
martin <s195469@student.dtu.dk>
2021-01-15 15:06:00 +0100

    Viewbinding implemented

--- app/src/main/java/dtu/android/moroapp/adapters/PremiumAdapter.kt
+++ app/src/main/java/dtu/android/moroapp/adapters/PremiumAdapter.kt
@@ -2,65 +2,42 @@ package dtu.android.moroapp.adapters
 
 import android.graphics.Color
 import android.graphics.PorterDuff
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import dtu.android.moroapp.FrontPageFragmentDirections
-import dtu.android.moroapp.R
-import dtu.android.moroapp.SingleEventFragment
+import dtu.android.moroapp.databinding.CardItemViewPagerBinding
 import dtu.android.moroapp.models.Event
-import kotlinx.android.synthetic.main.card_item_view_pager.view.*
-import kotlinx.android.synthetic.main.event_card_fragment.view.*
-import kotlinx.android.synthetic.main.event_viewpager.view.*
 
 class PremiumAdapter(
         val events: List<Event>
-): RecyclerView.Adapter<PremiumAdapter.EventViewHolder>(){
-    inner class EventViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+) : RecyclerView.Adapter<PremiumAdapter.EventViewHolder>() {
+    private lateinit var binding: CardItemViewPagerBinding
+
+    inner class EventViewHolder(itemView: CardItemViewPagerBinding) : RecyclerView.ViewHolder(itemView.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item_view_pager, parent, false)
-        return EventViewHolder(view)
+        val binding = CardItemViewPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        this.binding = binding
+        return EventViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val event = events[position]
-        holder.itemView.apply {
-            card_item_titel.text = event.title
-            card_item_time.text = event.getTimeToString()
+        binding.apply {
+            cardItemTitel.text = event.title
+            cardItemTime.text = event.getTimeToString()
             Picasso.get().load(event.image).fit().centerCrop().into(bannerID)
             bannerID.setColorFilter(Color.GRAY, PorterDuff.Mode.DARKEN)
 
-            bannerID.setOnClickListener {
+            root.setOnClickListener {
                 val action = FrontPageFragmentDirections
                         .actionFrontPageFragmentToSingleEventFragment(event)
-                findNavController().navigate(action);
+                root.findNavController().navigate(action);
             }
-            /*
-            title.text = event.title
-            description.text = event.genre
-            //date.text = event.getDate()
-            //time.text = event.getTime()
-            Picasso.get().load(event.image).fit().centerCrop().into(banner)
-            banner.setColorFilter(Color.GRAY, PorterDuff.Mode.DARKEN)
-            this.setOnClickListener{
-                val f = SingleEventFragment()
-                val fm = (context as AppCompatActivity).supportFragmentManager
-                val args = Bundle()
-                args.putSerializable("event",events[position])
-                f.arguments = args
-                fm.beginTransaction()
-                        .replace(R.id.mainFragment, f)
-                        .addToBackStack(null)
-                        .commit()
-            */}
+        }
     }
 
     override fun getItemCount(): Int {
